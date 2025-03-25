36c2e8eab2300c5248770f70591b994c96fa452f
Daniel Catão Moreira Licio <daniel.licio@saude.gov.br>
2025-03-25 11:20:19 -0300

    - commit inicial. Adicionando projeto criado no spring initialzr.

---
+++ src/main/java/br/com/unifecaf/gestao_livros/GestaoLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifecaf.gestao_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unifecaf/gestao_livros/GestaoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifecaf.gestao_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
